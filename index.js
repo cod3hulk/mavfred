e4e46830c767bcb3debde4489434ad558f2bc7be
Tomas Ave <cod3hulk@gmail.com>
2016-10-19 08:43:17 +0200

    Implemented workflow

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 'use strict';
 const alfy = require('alfy');
+const rp = require('request-promise');
 
-alfy.output([
-	{
-		title: 'Unicorn',
-		subtitle: alfy.input
-	}
-]);
+const options = {
+  uri: `https:\/\/search.maven.org/solrsearch/select?q=guice&rows=20&wt=json`,
+  qs: {
+    'q': alfy.input,
+    'rows': 20,
+    'wt': 'json'
+  },
+  json: true
+};
+
+rp(options)
+  .then(function(data) {
+    const items = data.response.docs.map(x => ({
+      title: x.id,
+      subtitle: x.latestVersion,
+      arg: `${x.g}|${x.a}|${x.latestVersion}|jar`
+    }));
+    alfy.output(items);
+  })
+  .catch(function(err) {
+    alfy.error(err);
+  });
